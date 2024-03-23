e34d4e2912af4781216b8875b5ef11917550f7d4
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-03-23 19:50:10 -0400

    simplified project structure

--- caffiend/settings.py
+++ caffiend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&&3)t8d_a3&t@7555=(f+wdz4g93-ddy&p%9%%iqb!*g_!@w&z'
+SECRET_KEY = 'django-insecure-o%#l6j#mwqzujp4yvk4#hi0ll7a^j$9b5u$n+!uy8xsiglc#u&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'users',
+    'tracker',
+]
 
 
 # Application definition
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'caffiend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Make sure 'templates' directory is included
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
