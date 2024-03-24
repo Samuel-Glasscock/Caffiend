e8621ddb3b2cdab3c3edef28e605de45f7550118
MMLewis1225 <62947613+MMLewis1225@users.noreply.github.com>
2024-03-24 02:02:42 -0400

    created reduce_intake page

--- tracker/views.py
+++ tracker/views.py
@@ -6,7 +6,16 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from users.models import Profile
+
 
 def reduce_intake_view(request):
-    # Your view logic here
-    return render(request, 'tracker/reduce_intake.html')
+    # Assuming the user is authenticated and their profile exists
+    user_profile = Profile.objects.get(user=request.user)
+    avg_caffeine_intake = user_profile.avg_caffeine_intake
+
+    context = {
+        'avg_caffeine_intake': avg_caffeine_intake,
+    }
+    return render(request, 'tracker/reduce_intake.html', context)
+
