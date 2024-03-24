74af0245129dc7fc6e1fdfe2d03ce8b16bd689af
MMLewis1225 <62947613+MMLewis1225@users.noreply.github.com>
2024-03-24 01:57:53 -0400

    created reduce_intake page

--- tracker/views.py
+++ tracker/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def reduce_intake_view(request):
     # Your view logic here
-    return HttpResponse("This is the reduce intake page")
+    return render(request, 'tracker/reduce_intake.html')
