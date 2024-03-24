4a40ac6de329ae0ed50cf60a6142d14641ac2d10
MMLewis1225 <62947613+MMLewis1225@users.noreply.github.com>
2024-03-24 02:20:08 -0400

    created reduce_intake page

--- users/forms.py
+++ users/forms.py
@@ -3,13 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class UserSignupForm(UserCreationForm):
-    
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('weight', 'avg_caffeine_intake', 'duration_number', 'duration_type')
+
+
+# forms.py
+class UpdateAvgCaffeineIntakeForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avg_caffeine_intake']
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import UserLoginView, UserLogoutView, ProfileView, signup
+
+from . import views
+from .views import UserLoginView, UserLogoutView, ProfileView, signup, profile
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(next_page='home'), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('signup/', signup, name='signup'),
+    path('profile/', profile, name='profile'),
+    path('update_avg_caffeine_intake/', views.update_avg_caffeine_intake, name='update_avg_caffeine_intake'),
 ]
+
+# forms.py
+
+# urls.py
