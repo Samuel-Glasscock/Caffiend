9818958d9e9527cec85203f50247a15284283641
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-03-24 00:52:06 -0400

    added signup page and redirects

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserLoginView, UserLogoutView, ProfileView
+from .views import UserLoginView, UserLogoutView, ProfileView, signup
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(next_page='home'), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('signup/', signup, name='signup'),
 ]
